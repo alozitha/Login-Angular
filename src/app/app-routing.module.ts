8cd62ecf67c2436ca4cc89c9145792812bdf9a57
alozitha <alondrav258@gmail.com>
2019-12-15 15:01:27 -0600

    Anadimos navbar y turtas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './componets/login/login.component';
+import{RegisterPageComponent} from './componets/register-page/register-page.component';
+import{PrivatePageComponent} from './componets/private-page/private-page.component';
+import{NotFoundPageComponent} from './componets/not-found-page/not-found-page.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  //definimos las rutas
+  	{path:'',component:LoginComponent},
+    {path:'login',component:LoginComponent},
+    {path:'register',component:RegisterPageComponent},
+    {path:'private',component:PrivatePageComponent},
+    {path:"**",component:NotFoundPageComponent}
 ];
 
 @NgModule({
