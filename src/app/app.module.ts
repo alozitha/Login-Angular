2554865575b1b09adc7f1f68bc47a9f61beb8fb7
alozitha <alondrav258@gmail.com>
2019-12-15 14:18:30 -0600

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componets/login/login.component';
+import { NavbarComponent } from './componets/navbar/navbar.component';
+import { RegisterPageComponent } from './componets/register-page/register-page.component';
+import { PrivatePageComponent } from './componets/private-page/private-page.component';
+import { NotFoundPageComponent } from './componets/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    RegisterPageComponent,
+    PrivatePageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
